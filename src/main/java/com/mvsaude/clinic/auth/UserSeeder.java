5a7653638d6d549c7827bfa4f0b640c2346e133e
tn-junior <juju2004.tary@gmail.com>
2025-11-28 15:11:03 -0300

    chore: adiciona seed inicial de usuários

--- src/main/java/com/mvsaude/clinic/auth/UserSeeder.java
+++ src/main/java/com/mvsaude/clinic/auth/UserSeeder.java
@@ -12,18 +12,24 @@ import org.springframework.stereotype.Component;
 @Component @RequiredArgsConstructor
 public class UserSeeder {
     private final UsuarioRepository repo;
-    private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+    private final BCryptPasswordEncoder encoder;
 
 
     @PostConstruct
     public void seed() {
         if (repo.findByUsername("admin").isEmpty()) {
-            repo.save(Usuario.builder().username("admin").password(encoder.encode("admin"))
-                    .role(Role.ROLE_ADMIN).build());
+            repo.save(Usuario.builder()
+                    .username("admin")
+                    .password(encoder.encode("admin"))
+                    .role(Role.ROLE_ADMIN)
+                    .build());
         }
         if (repo.findByUsername("user").isEmpty()) {
-            repo.save(Usuario.builder().username("user").password(encoder.encode("user"))
-                    .role(Role.ROLE_USER).build());
+            repo.save(Usuario.builder()
+                    .username("user")
+                    .password(encoder.encode("user"))
+                    .role(Role.ROLE_USER)
+                    .build());
         }
     }
 }
